cf7fa379a4c09bc093c4472eed30a7740cbaa104
Yoon Hee Ha <yoonhee@bearrobotics.ai>
2025-03-26 12:55:39 -0400

    update Amplify from gen 1 to gen 2

--- yoonhee-app/next.config.js
+++ yoonhee-app/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // Add this for better static file serving
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
   /* config options here */
 };
 
